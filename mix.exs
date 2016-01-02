f7d752baa9f70293c3d7fb50cef7da4a5a66b7f3
Daniel Neighman <has.sox@gmail.com>
2016-01-01 22:14:31 -0800

    Fill out some more of the hex info

--- mix.exs
+++ mix.exs
@@ -1,37 +1,53 @@
 defmodule StdJsonIo.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/hassox/std_json_io"
+  @maintainers ["Daniel Neighman"]
+
   def project do
     [app: :std_json_io,
-     version: "0.1.0",
+     name: "StdJsonIo",
+     version: @version,
+     package: @package,
      elixir: "~> 1.1",
+     source_url: @url,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     maintainers: @maintainers,
+     description: "Application for managing and communicating with IO servers via JSON",
+     homepage_url: @url,
+     docs: docs,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :porcelain]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  def docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      source_ref: "v#{@version}",
+      main: "extra-readme"
+    ]
+  end
+
   defp deps do
     [
       {:porcelain, "~> 2.0"},
       {:poolboy, "~> 1.5.1"},
       {:poison, "~> 1.5.0"},
       {:fs, "~> 0.9.1"},
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: @maintainers,
+      licenses: ["MIT"],
+      links: %{github: @url},
+      files: ~w(lib) ++ ~w(CHANGELOG.md LICENSE mix.exs README.md)
     ]
   end
 end
